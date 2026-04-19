45c923a739b53c6faa5f1017122a1a3fa3979161
Lakalot <188421644+Lakalot@users.noreply.github.com>
2026-04-19 13:37:37 +0200

    feat(firmware): add BleTransportV5 frame dispatcher

---
+++ firmware/esp32/lib/esp-control-ble/src/protocol/ProtocolV5.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+
+#define ECB_V5_DATA_CHAR_UUID "12345678-1234-1234-1234-123456789abf"
+
+namespace ecb { namespace v5 {
+static constexpr uint16_t kManifestChunkSize = 180;
+static constexpr uint16_t kMaxFrameBody = 512;
+}} // namespace
